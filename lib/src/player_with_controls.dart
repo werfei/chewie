85571b9dff84024cc976c36ba4b78fd04952edb2
王二飞 <459876292@qq.com>
2020-12-30 19:38:30 +0800

    aaa

--- lib/src/player_with_controls.dart
+++ lib/src/player_with_controls.dart
@@ -26,12 +26,7 @@ class PlayerWithControls extends StatelessWidget {
       BuildContext context,
       ChewieController chewieController,
     ) {
-      final controls = Theme.of(context).platform == TargetPlatform.android
-          ? const MaterialControls()
-          : const CupertinoControls(
-              backgroundColor: Color.fromRGBO(41, 41, 41, 0.7),
-              iconColor: Color.fromARGB(255, 200, 200, 200),
-            );
+      final controls =  const MaterialControls();
       return chewieController.showControls ? chewieController.customControls ?? controls : Container();
     }
 
